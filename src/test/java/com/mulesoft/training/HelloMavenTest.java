09185dbf876f5fe3f95a9e7f793a431a74334dee
Sai Krishna <nkvrpkrishna@gmail.com>
2017-11-13 17:53:20 -0600

    Fixed Issues

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,16 +8,16 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-//    @Test
-//    public void mavenFlowReturnsHelloMaven() throws Exception {
-//        runFlowAndExpect("mavenFlow", "Hello Maven");
-//    }
     @Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+/*    @Test
     public void retrieveFlightsAddsAppropriateHeader() throws Exception {
       MuleEvent event = runFlow("retrieveFlights");
       String contentType = event.getMessage().getOutboundProperty("Content-Type");
       assertEquals("application/json", contentType);
-    }
+    }*/
     
     @Override
     protected String getConfigFile() {
